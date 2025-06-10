9eb9ead4da5cfc5ce2b72ca8696162f6e246f380
Jesus Alberto Lebron ROA <eustanaciomt77@gmail.com>
2025-06-09 21:56:03 -0400

    Formulario 2 realizado

---
+++ Formulario 2/Formulario 2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbxLongitud_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            int longitudPass = int.Parse(tbLongitud.Text);
+
+            Random random = new Random();
+
+            string cadena = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+
+            char[] Password = new char[longitudPass];
+
+            for (int i = 0; i < longitudPass; i++) { 
+
+                int indice = random.Next(cadena.Length);
+                Password[i] = cadena[indice];
+            }
+            string NuevoPass = new string(Password);
+            tbGenerada.Text = NuevoPass;
+        }
+    }
+}
