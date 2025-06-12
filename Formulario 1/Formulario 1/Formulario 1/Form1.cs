1f5a42acf5710769cb5b6a0073effdf4730aa528
Jesus Alberto Lebron ROA <eustanaciomt77@gmail.com>
2025-06-12 15:53:31 -0400

    Ejercicio 1 de la Unidad 3: Tabla de Multiplicación
    Ejercicio 1 de la Unidad 3: Tabla de Multiplicación

---
+++ Formulario 1/Formulario 1/Formulario 1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unidad_3.Actividad_Práctica_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btngenerar_Click(object sender, EventArgs e)
+        {
+            lbtabla.Items.Clear();
+
+            if (int.TryParse(tbnumeros.Text, out int numero))
+            {
+                for (int i = 1; i <= 12; i++)
+                {
+                    int producto = numero * i;
+                    lbtabla.Items.Add($"{numero} x {i} = {producto}");
+
+                }
+
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
