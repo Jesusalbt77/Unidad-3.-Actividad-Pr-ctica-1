d8518221f034fa672c1a34e30cd53c78fba0691e
Jesus Alberto Lebron ROA <eustanaciomt77@gmail.com>
2025-06-11 22:48:11 -0400

    Ejercicio 2  de unidad 3 realizado

--- Formulario 2/Formulario 2/Form1.Designer.cs
+++ Formulario 2/Formulario 2/Form1.Designer.cs
@@ -28,74 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tbxLongitud = new System.Windows.Forms.Label();
-            this.btnGenerar = new System.Windows.Forms.Button();
-            this.tbLongitud = new System.Windows.Forms.TextBox();
-            this.tbGenerada = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            this.cbminutos = new System.Windows.Forms.ComboBox();
+            this.cbsegundos = new System.Windows.Forms.ComboBox();
+            this.lbtiempo = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btniniciar = new System.Windows.Forms.Button();
+            this.btndetener = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // tbxLongitud
+            // cbminutos
             // 
-            this.tbxLongitud.AutoSize = true;
-            this.tbxLongitud.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbxLongitud.Location = new System.Drawing.Point(128, 107);
-            this.tbxLongitud.Name = "tbxLongitud";
-            this.tbxLongitud.Size = new System.Drawing.Size(79, 19);
-            this.tbxLongitud.TabIndex = 0;
-            this.tbxLongitud.Text = "Longitud";
-            this.tbxLongitud.Click += new System.EventHandler(this.tbxLongitud_Click);
+            this.cbminutos.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbminutos.FormattingEnabled = true;
+            this.cbminutos.Location = new System.Drawing.Point(191, 84);
+            this.cbminutos.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.cbminutos.Name = "cbminutos";
+            this.cbminutos.Size = new System.Drawing.Size(150, 26);
+            this.cbminutos.TabIndex = 0;
             // 
-            // btnGenerar
+            // cbsegundos
             // 
-            this.btnGenerar.BackColor = System.Drawing.Color.Tomato;
-            this.btnGenerar.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGenerar.Location = new System.Drawing.Point(106, 178);
-            this.btnGenerar.Name = "btnGenerar";
-            this.btnGenerar.Size = new System.Drawing.Size(117, 34);
-            this.btnGenerar.TabIndex = 1;
-            this.btnGenerar.Text = "Generar";
-            this.btnGenerar.UseVisualStyleBackColor = false;
-            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            this.cbsegundos.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbsegundos.FormattingEnabled = true;
+            this.cbsegundos.Location = new System.Drawing.Point(441, 84);
+            this.cbsegundos.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.cbsegundos.Name = "cbsegundos";
+            this.cbsegundos.Size = new System.Drawing.Size(150, 26);
+            this.cbsegundos.TabIndex = 1;
             // 
-            // tbLongitud
+            // lbtiempo
             // 
-            this.tbLongitud.Location = new System.Drawing.Point(254, 104);
-            this.tbLongitud.Name = "tbLongitud";
-            this.tbLongitud.Size = new System.Drawing.Size(152, 22);
-            this.tbLongitud.TabIndex = 2;
+            this.lbtiempo.AutoSize = true;
+            this.lbtiempo.Font = new System.Drawing.Font("Arial", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbtiempo.Location = new System.Drawing.Point(308, 147);
+            this.lbtiempo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbtiempo.Name = "lbtiempo";
+            this.lbtiempo.Size = new System.Drawing.Size(145, 55);
+            this.lbtiempo.TabIndex = 2;
+            this.lbtiempo.Text = "00:00";
+            this.lbtiempo.Click += new System.EventHandler(this.btniniciar_Click);
             // 
-            // tbGenerada
+            // label2
             // 
-            this.tbGenerada.Location = new System.Drawing.Point(254, 185);
-            this.tbGenerada.Name = "tbGenerada";
-            this.tbGenerada.Size = new System.Drawing.Size(152, 22);
-            this.tbGenerada.TabIndex = 3;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(187, 52);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Minutos:";
             // 
-            // label1
+            // label3
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Blue;
-            this.label1.Location = new System.Drawing.Point(177, 23);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(376, 33);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Generador de Contraseñas";
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(437, 52);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Segundos:";
+            // 
+            // btniniciar
+            // 
+            this.btniniciar.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btniniciar.Location = new System.Drawing.Point(191, 253);
+            this.btniniciar.Name = "btniniciar";
+            this.btniniciar.Size = new System.Drawing.Size(150, 45);
+            this.btniniciar.TabIndex = 5;
+            this.btniniciar.Text = "Iniciar";
+            this.btniniciar.UseVisualStyleBackColor = true;
+            this.btniniciar.Click += new System.EventHandler(this.btniniciar_Click);
+            // 
+            // btndetener
+            // 
+            this.btndetener.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndetener.Location = new System.Drawing.Point(441, 253);
+            this.btndetener.Name = "btndetener";
+            this.btndetener.Size = new System.Drawing.Size(150, 45);
+            this.btndetener.TabIndex = 6;
+            this.btndetener.Text = "Detener";
+            this.btndetener.UseVisualStyleBackColor = true;
+            this.btndetener.Click += new System.EventHandler(this.btndetener_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.tbGenerada);
-            this.Controls.Add(this.tbLongitud);
-            this.Controls.Add(this.btnGenerar);
-            this.Controls.Add(this.tbxLongitud);
+            this.ClientSize = new System.Drawing.Size(1000, 478);
+            this.Controls.Add(this.btndetener);
+            this.Controls.Add(this.btniniciar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbtiempo);
+            this.Controls.Add(this.cbsegundos);
+            this.Controls.Add(this.cbminutos);
+            this.Font = new System.Drawing.Font("Vivaldi", 8.25F);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.timer1_Tick_1);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,11 +145,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Label tbxLongitud;
-        private System.Windows.Forms.Button btnGenerar;
-        private System.Windows.Forms.TextBox tbLongitud;
-        private System.Windows.Forms.TextBox tbGenerada;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbminutos;
+        private System.Windows.Forms.ComboBox cbsegundos;
+        private System.Windows.Forms.Label lbtiempo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btniniciar;
+        private System.Windows.Forms.Button btndetener;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
