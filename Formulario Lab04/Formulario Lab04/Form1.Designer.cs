ce8ae215ece344e57807fed38aea8c01a83067e2
Jesus Alberto Lebron ROA <eustanaciomt77@gmail.com>
2025-06-15 14:53:44 -0400

    Formulario del 4to video (Lab04) de la Unidad 3
    Formulario donde se lleva a cabo el registro, nombres y promedios de diferentes alumnos

---
+++ Formulario Lab04/Formulario Lab04/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Formulario_Lab04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbCodigo = new System.Windows.Forms.TextBox();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbPromedio = new System.Windows.Forms.TextBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbBuscar = new System.Windows.Forms.TextBox();
+            this.dgAlumnos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgAlumnos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(63, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(63, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Promedio:";
+            // 
+            // tbCodigo
+            // 
+            this.tbCodigo.Location = new System.Drawing.Point(165, 62);
+            this.tbCodigo.Name = "tbCodigo";
+            this.tbCodigo.Size = new System.Drawing.Size(191, 22);
+            this.tbCodigo.TabIndex = 3;
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(165, 118);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(191, 22);
+            this.tbNombre.TabIndex = 4;
+            // 
+            // tbPromedio
+            // 
+            this.tbPromedio.Location = new System.Drawing.Point(165, 168);
+            this.tbPromedio.Name = "tbPromedio";
+            this.tbPromedio.Size = new System.Drawing.Size(191, 22);
+            this.tbPromedio.TabIndex = 5;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.Location = new System.Drawing.Point(65, 219);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(101, 36);
+            this.btnRegistrar.TabIndex = 6;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(222, 219);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(101, 36);
+            this.btnEliminar.TabIndex = 7;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrdenar.Location = new System.Drawing.Point(379, 219);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(101, 36);
+            this.btnOrdenar.TabIndex = 8;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(379, 295);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(101, 36);
+            this.btnBuscar.TabIndex = 9;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(45, 304);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 19);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Buscar por código:";
+            // 
+            // tbBuscar
+            // 
+            this.tbBuscar.Location = new System.Drawing.Point(239, 303);
+            this.tbBuscar.Name = "tbBuscar";
+            this.tbBuscar.Size = new System.Drawing.Size(134, 22);
+            this.tbBuscar.TabIndex = 11;
+            // 
+            // dgAlumnos
+            // 
+            this.dgAlumnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgAlumnos.Location = new System.Drawing.Point(49, 340);
+            this.dgAlumnos.Name = "dgAlumnos";
+            this.dgAlumnos.RowHeadersWidth = 51;
+            this.dgAlumnos.RowTemplate.Height = 24;
+            this.dgAlumnos.Size = new System.Drawing.Size(470, 150);
+            this.dgAlumnos.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 530);
+            this.Controls.Add(this.dgAlumnos);
+            this.Controls.Add(this.tbBuscar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.tbPromedio);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.tbCodigo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Registro de alumnos";
+            ((System.ComponentModel.ISupportInitialize)(this.dgAlumnos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbCodigo;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbPromedio;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbBuscar;
+        private System.Windows.Forms.DataGridView dgAlumnos;
+    }
+}
+
---
+++ Formulario Lab04/Formulario Lab04/Form1.cs
@@ -0,0 +1,118 @@
+﻿using Formulario_Lab04.Controllers;
+using Formulario_Lab04.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario_Lab04
+{
+    public partial class Form1 : Form
+    {
+        // Llamar al controlador 
+        private AlumnoController alumnoController = new AlumnoController();
+
+        public string Codigo { get; private set; }
+        public string Nombre { get; private set; }
+        public double Promedio { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Mostrar
+        private void MostrarAlumnos(Alumno[] alumnos)
+        {
+            dgAlumnos.DataSource = null;
+            dgAlumnos.DataSource = alumnos;
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            // Validar campos de entrada
+            if (tbCodigo.Text == "" || tbNombre.Text == "" || tbPromedio.Text == "")
+            {
+                MessageBox.Show("Ingrese todos los campos");
+                return;
+            }
+
+            // Crear el alumno
+            Alumno alumno = new Alumno()
+            {
+                Codigo = tbCodigo.Text,
+                Nombre = tbNombre.Text,
+                Promedio = double.Parse(tbPromedio.Text)
+            };
+
+            {
+                // Registrar alumno en el arreglo
+                alumnoController.Registrar(alumno);
+
+                // Mostrar los alumnos
+                MostrarAlumnos(alumnoController.ListarTodo());
+
+                limpiarCampos();
+                {
+                }
+            }
+        }
+        public void limpiarCampos()
+        {
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+           // Validación de seleccion de fila
+            if(dgAlumnos.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Seleccione registro para eliminar");
+                return;
+            }
+
+            // Obtener el código
+            String codigo = dgAlumnos.SelectedRows[0].Cells[0].Value.ToString();
+
+            // Eliminar la fila seleccionafa
+            alumnoController.Eliminar(codigo);
+
+            // Mostrar
+            MostrarAlumnos(alumnoController.ListarTodo());
+
+        }
+
+        private void btnOrdernar_Click(object sender, EventArgs e)
+        {
+            MostrarAlumnos(alumnoController.Ordenar());
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            // Validar campo
+            if (tbBuscar.Text == "")
+            {
+                MessageBox.Show("Ingrese el código a buscar");
+                return;
+            }
+            // Obtener el código
+            String codigo = tbBuscar.Text;
+            // Mostrar
+            MostrarAlumnos(alumnoController.BuscarPorCodigo(codigo));
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            MostrarAlumnos(alumnoController.Ordenar());
+        }
+    }
+}         
+        
+    
+
+          
+
