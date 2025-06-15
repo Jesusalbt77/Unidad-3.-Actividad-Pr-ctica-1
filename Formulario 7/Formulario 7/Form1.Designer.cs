6afc807a1f2411abaf6d7920b745cddb720ce2de
Jesus Alberto Lebron ROA <eustanaciomt77@gmail.com>
2025-06-15 10:23:46 -0400

    Formulario 7 Unidad 3: Agrega Números Primos en un ListBox
    Agrega los Números Primos en una ListBox

---
+++ Formulario 7/Formulario 7/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Formulario_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInicio = new System.Windows.Forms.TextBox();
+            this.txtFin = new System.Windows.Forms.TextBox();
+            this.listBoxPrimos = new System.Windows.Forms.ListBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInicio
+            // 
+            this.txtInicio.Location = new System.Drawing.Point(176, 161);
+            this.txtInicio.Name = "txtInicio";
+            this.txtInicio.Size = new System.Drawing.Size(100, 22);
+            this.txtInicio.TabIndex = 0;
+            // 
+            // txtFin
+            // 
+            this.txtFin.Location = new System.Drawing.Point(358, 161);
+            this.txtFin.Name = "txtFin";
+            this.txtFin.Size = new System.Drawing.Size(100, 22);
+            this.txtFin.TabIndex = 1;
+            // 
+            // listBoxPrimos
+            // 
+            this.listBoxPrimos.FormattingEnabled = true;
+            this.listBoxPrimos.ItemHeight = 16;
+            this.listBoxPrimos.Location = new System.Drawing.Point(266, 211);
+            this.listBoxPrimos.Name = "listBoxPrimos";
+            this.listBoxPrimos.Size = new System.Drawing.Size(124, 180);
+            this.listBoxPrimos.TabIndex = 2;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnGenerar.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerar.Location = new System.Drawing.Point(28, 178);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(120, 50);
+            this.btnGenerar.TabIndex = 3;
+            this.btnGenerar.Text = "Generar Primos";
+            this.btnGenerar.UseVisualStyleBackColor = false;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(106, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(489, 40);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Formulario de Números Primos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(173, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Inicio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(357, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Fin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.listBoxPrimos);
+            this.Controls.Add(this.txtFin);
+            this.Controls.Add(this.txtInicio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInicio;
+        private System.Windows.Forms.TextBox txtFin;
+        private System.Windows.Forms.ListBox listBoxPrimos;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Formulario 7/Formulario 7/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            listBoxPrimos.Items.Clear();
+
+            if (int.TryParse(txtInicio.Text, out int inicio) && int.TryParse(txtFin.Text, out int fin))
+            {
+                if (inicio > fin)
+                {
+                    MessageBox.Show("El valor de inicio debe ser menor o igual al valor de fin.");
+                    return;
+                }
+
+                for (int i = inicio; i <= fin; i++)
+                {
+                    if (EsPrimo(i))
+                        listBoxPrimos.Items.Add(i);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, introduce valores numéricos válidos.");
+            }
+        }
+
+        private bool EsPrimo(int numero)
+        {
+            if (numero < 2) return false;
+            for (int i = 2; i <= Math.Sqrt(numero); i++)
+            {
+                if (numero % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
