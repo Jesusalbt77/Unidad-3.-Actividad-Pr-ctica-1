793f63b1b391a9776b123c2315e964609be8bfba
Jesus Alberto Lebron ROA <eustanaciomt77@gmail.com>
2025-06-14 23:37:00 -0400

    Formulario 8 de la Unidad 3: Calculador de Palabras
    Calcula cuantas palabras contiene una oración

---
+++ Formulario 8/Formulario 8/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Formulario_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnContar = new System.Windows.Forms.Button();
+            this.txtOracion = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(211, 91);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(196, 19);
+            this.lblResultado.TabIndex = 0;
+            this.lblResultado.Text = "Cantidad de Palabras: 0";
+            // 
+            // btnContar
+            // 
+            this.btnContar.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContar.Location = new System.Drawing.Point(242, 203);
+            this.btnContar.Name = "btnContar";
+            this.btnContar.Size = new System.Drawing.Size(131, 53);
+            this.btnContar.TabIndex = 1;
+            this.btnContar.Text = "Contar Palabras";
+            this.btnContar.UseVisualStyleBackColor = true;
+            this.btnContar.Click += new System.EventHandler(this.btnContar_Click);
+            // 
+            // txtOracion
+            // 
+            this.txtOracion.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOracion.Location = new System.Drawing.Point(161, 122);
+            this.txtOracion.Multiline = true;
+            this.txtOracion.Name = "txtOracion";
+            this.txtOracion.Size = new System.Drawing.Size(300, 60);
+            this.txtOracion.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtOracion);
+            this.Controls.Add(this.btnContar);
+            this.Controls.Add(this.lblResultado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnContar;
+        private System.Windows.Forms.TextBox txtOracion;
+    }
+}
+
